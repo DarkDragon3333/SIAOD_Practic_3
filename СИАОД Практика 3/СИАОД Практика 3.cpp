98b3c6a9d3ed78b46cdb79710df57f2cc88dd9bd
DarkDragon3333 <minaichiev-dragon2017@yandex.ru>
2022-10-18 18:59:03 +0300

    Добавьте файлы проекта.

---
+++ СИАОД Практика 3/СИАОД Практика 3.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include "Search.h"
+
+using namespace std;
+
+//void increaseStr(char* &str, int &size) {
+//	char* newStr = new char[size * 2];
+//	for (int i = 0; i < size; i++) {
+//		newStr[i] = str[i];
+//	}
+//	cout << endl << sizeof(*str);
+//	delete[] str;
+//	*str = *newStr;
+//	cout << endl << sizeof(*str);
+//}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	ofstream File;
+	string stroka;
+
+	int strSize = 0;
+	int i = 0;
+
+	File.open("try.txt");
+	File.clear();
+	if (!File.is_open()) {
+		cout << "Not found";
+	}
+	else {
+		string s = "Привет,меня,зовут,Максим,А,как,зовут,тебя,";
+		strSize = s.length();
+		File << s;
+		File.close();
+
+		ifstream Read("try.txt");
+		getline(Read, stroka);
+		Read.close();
+	}
+
+	for (int i = 0; i < strSize; i++)
+	{
+		if (stroka[i] >= 'А' && stroka[i] <= 'Я') {
+			
+		}
+		else if (stroka[i] == ',') {
+			stroka[i] = ' ';
+			
+		}
+		cout << stroka[i];
+	}
+	cout << endl << stroka;
+	
+	return 0;
+}
