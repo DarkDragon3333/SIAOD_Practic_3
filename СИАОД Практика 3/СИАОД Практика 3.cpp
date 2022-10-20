1e2b7ed115ad89b27b4b35154b649ed82e6c31f4
Максим Минайчев <minaichiev-dragon2017@yandex.ru>
2022-10-20 22:44:53 +0300

    Ready

--- СИАОД Практика 3/СИАОД Практика 3.cpp
+++ СИАОД Практика 3/СИАОД Практика 3.cpp
@@ -1,66 +1,86 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 #include "Search.h"
 
 using namespace std;
 
-//void increaseStr(char* &str, int &size) {
-//	char* newStr = new char[size * 2];
-//	for (int i = 0; i < size; i++) {
-//		newStr[i] = str[i];
-//	}
-//	cout << endl << sizeof(*str);
-//	delete[] str;
-//	*str = *newStr;
-//	cout << endl << sizeof(*str);
-//}
+vector<int> prefix_function(string s) {
+	int n = (int)s.length();
+	vector<int> pi(n);
+	for (int i = 1; i < n; ++i) {
+		int j = pi[i - 1];
+		while (j > 0 && s[i] != s[j])
+			j = pi[j - 1];
+		if (s[i] == s[j])  ++j;
+		pi[i] = j;
+	}
+	for (size_t i = 0; i < pi.size(); i++)
+	{
+		cout << pi[i];
+	}
+	return pi;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	char stroca[1024] = "Hi,my,name-is.Max:What!is?your;name?" ;
+	char stroca[1024] = "Hi,my,name-is.max:What!is?your;name?My,name.is;jeck.How;old,are.you?" ;
 	char stroca_2[1024];
-
-	int* arr = new int[10];
-	int* arrt = new int[11];
-
-	for (size_t i = 0; i < 4; i++)
-	{
-
-	}
+	
 
 	for (size_t i = 0; i < sizeof(stroca); i++)
 	{
 		stroca_2[i] = stroca[i];
 	}
-	
 
-	for (int i = 0; i < sizeof(stroca); i++)
+
+	for (size_t i = 0; i < sizeof(stroca); i++)
 	{
-		if ( (stroca[i] == '.' or stroca[i] == '!' or stroca[i] == '?') and stroca[i + 1] == '\0') {
+		if ( (stroca[i] == '.' || stroca[i] == '!' || stroca[i] == '?') && stroca[i + 1] == '\0') {
 			break;
 		}
-		else if ( (stroca[i + 1] >= 'A' && stroca[i + 1] <= 'Z') and  ((stroca[i] >= '!' && stroca[i] <= '/') or  (stroca[i] >= ':' && stroca[i] <= '?')) and stroca[i + 1] != '\0' and stroca[i + 2] != '\0') {
-			for (size_t j = 0; j <= i; j++)
+		else if ((stroca[i] >= '!' && stroca[i] < '0') || (stroca[i] >= ':' && stroca[i] < 'A'))  {
+			stroca[i] = ' ';
+		}
+	}
+
+	cout << endl << stroca;
+
+	int index = 0;
+	for (size_t i = index; i < sizeof(stroca); i++)
+	{
+		index++;
+		if (stroca[i + 1] >= 'A' && stroca[i + 1] <= 'Z' && !(stroca_2[i - 2] == '.' && stroca_2[i - 1] == ' ' && stroca_2[i] == ' ')) {
+			
+			for (size_t j = 0; j < i; j++) 
 			{
 				stroca_2[j] = stroca[j];
 			}
-			
+
+			stroca_2[i] = '.';
 			stroca_2[i + 1] = ' ';
 			stroca_2[i + 2] = ' ';
 
-			for (size_t j = i + 3; j <= sizeof(stroca); j++)
+			index += 3;
+			
+			for (size_t j = (i + 3); j < sizeof(stroca); j++)
 			{
-				stroca_2[j] = stroca[j];
+				stroca_2[j] = stroca[j - 2];
 			}
-		}
-		else if ((stroca[i] >= '!' && stroca[i] < '0') or (stroca[i] >= ':' && stroca[i] < 'A')) {
-			stroca[i] = ' ';
-		}
-		cout << stroca_2[i];
+
+			for (size_t i = 0; i < sizeof(stroca); i++)
+			{
+				stroca[i] = stroca_2[i];
+			}
+		}	
 	}
-	cout << endl << stroca_2;
+
+	string s = "abcabcd";
+
+	cout << endl << stroca_2 << endl;
 	
+	prefix_function(s);
 	return 0;
 }
