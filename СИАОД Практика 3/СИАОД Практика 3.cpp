ed1e577f0a93132ea459deb2357781205abd5c66
Максим Минайчев <minaichiev-dragon2017@yandex.ru>
2022-10-20 23:10:20 +0300

    Ready

--- СИАОД Практика 3/СИАОД Практика 3.cpp
+++ СИАОД Практика 3/СИАОД Практика 3.cpp
@@ -4,38 +4,40 @@
 
 using namespace std;
 
-vector<int> prefix_function(string s) {
-	int n = (int)s.length();
-	vector<int> pi(n);
+//Префикс функция
+vector<int> prefix_function(string s) 
+{ 
+	int n = (int)s.length(); //Для итерации по вектору
+	vector<int> pi(n); //Место хранения значений префикс функции
+
+	//Дальше какая-то хрень. Смотрите в текстовике ссылки
 	for (int i = 1; i < n; ++i) {
 		int j = pi[i - 1];
 		while (j > 0 && s[i] != s[j])
 			j = pi[j - 1];
-		if (s[i] == s[j])  ++j;
+		if (s[i] == s[j])  
+			++j;
 		pi[i] = j;
 	}
-	for (size_t i = 0; i < pi.size(); i++)
+
+	cout << endl << "Значение префикс функции: ";
+	for (size_t i = 0; i < pi.size(); i++) //Вывод значений префикс функции
 	{
 		cout << pi[i];
 	}
-	return pi;
+	return pi; //Можно, в принципе, не возвращать вектор, а просто в функции через цикл вывести значение вектора pi.
 }
 
-int main()
+void main()
 {
 	setlocale(LC_ALL, "ru");
 
-	char stroca[1024] = "Hi,my,name-is.max:What!is?your;name?My,name.is;jeck.How;old,are.you?" ;
-	char stroca_2[1024];
-	
+	char stroca[1024] = "Hi,my,name-is.max:What!is?your;name?My,name.is;jeck.How;old,are.you?I.am;18" ; //Рандомный текст для первой части задания
+	char stroca_2[1024]; //Вспомогательная строка
 
-	for (size_t i = 0; i < sizeof(stroca); i++)
-	{
-		stroca_2[i] = stroca[i];
-	}
+	cout << endl << stroca; //Выводим первую строку в качестве изначального образца
 
-
-	for (size_t i = 0; i < sizeof(stroca); i++)
+	for (size_t i = 0; i < sizeof(stroca); i++) //Чистим 1-ю строку от знаков препинания, кроме последнего
 	{
 		if ( (stroca[i] == '.' || stroca[i] == '!' || stroca[i] == '?') && stroca[i + 1] == '\0') {
 			break;
@@ -45,41 +47,38 @@ int main()
 		}
 	}
 
-	cout << endl << stroca;
+	cout << endl << stroca; //Выводим чистую первую строку
 
-	int index = 0;
-	for (size_t i = index; i < sizeof(stroca); i++)
+	for (size_t i = 0; i < sizeof(stroca); i++) //Итерируемся по 
 	{
-		index++;
+		//Если предыдущие символы не ' ', ' ', '.' и следующий символ заглавная буква то:
 		if (stroca[i + 1] >= 'A' && stroca[i + 1] <= 'Z' && !(stroca_2[i - 2] == '.' && stroca_2[i - 1] == ' ' && stroca_2[i] == ' ')) {
 			
 			for (size_t j = 0; j < i; j++) 
 			{
-				stroca_2[j] = stroca[j];
+				stroca_2[j] = stroca[j]; //Заполняем вспомогательную строку до пробела и заглавной буквы
 			}
-
+			//Вставляем знак препинания и 2 пробела
 			stroca_2[i] = '.';
 			stroca_2[i + 1] = ' ';
 			stroca_2[i + 2] = ' ';
-
-			index += 3;
 			
-			for (size_t j = (i + 3); j < sizeof(stroca); j++)
+			 i += 3; //Перескакиваем через вставленых 3 символа
+			
+			for (size_t j = i; j < sizeof(stroca); j++)
 			{
-				stroca_2[j] = stroca[j - 2];
+				stroca_2[j] = stroca[j - 2]; //Дозаполняем вспомогательную строку
 			}
 
 			for (size_t i = 0; i < sizeof(stroca); i++)
 			{
-				stroca[i] = stroca_2[i];
+				stroca[i] = stroca_2[i]; //Итоговый результат копируем в 1-ую строку
 			}
 		}	
 	}
 
-	string s = "abcabcd";
+	cout << endl << stroca_2 << endl; //Вывод финального образца
 
-	cout << endl << stroca_2 << endl;
-	
-	prefix_function(s);
-	return 0;
+	string s = "abcabcd"; //Рандомная строка для второй части задания
+	prefix_function(s); //Вызов префикс функции
 }
